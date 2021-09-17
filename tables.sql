c2a1a2beff54b2cc672204e8bf7ca7b272e3c764
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-09-17 17:56:14 -0400

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -48,3 +48,30 @@ uuid VARCHAR(255),
 	reducing VARCHAR(255), 
 	nh3 VARCHAR(255), 
 	gasko VARCHAR(255));
+
+CREATE TABLE iotjetsonjson 
+(
+	uuid VARCHAR(255), 
+	camera VARCHAR(255),
+	ipaddress VARCHAR(255),  
+	networktime VARCHAR(255), 
+        top1pct VARCHAR(255), 
+	top1 VARCHAR(255), 
+	cputemp VARCHAR(255), 
+	gputemp VARCHAR(255),
+        gputempf VARCHAR(255),
+	cputempf VARCHAR(255), 
+	runtime VARCHAR(255),
+	filename VARCHAR(255),  
+	host_name VARCHAR(255), 
+        macaddress VARCHAR(255), 
+	end VARCHAR(255), 
+	te VARCHAR(255), 
+	systemtime VARCHAR(255),
+        diskusage VARCHAR(255),
+	memory VARCHAR(255), 
+	id VARCHAR(255), 
+	imageinput VARCHAR(255)
+);
+	
+	
